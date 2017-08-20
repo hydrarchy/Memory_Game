c7a0a52ad1c8bbf0fa7fa537d51bcbbb01280c6c
Bruce Baisch Jr <bbaisch@gmu.edu>
2017-08-20 16:38:10 -0400

    Add content to back of squares
    Add shuffle function to shuffle
    array of numbers and return new
    array.  Add each element of new
    array as new div inside each div
    with class "back" when page loads.

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
 
+  function shuffle(array) {
+    var currentIndex = array.length
+      , temporaryValue
+      , randomIndex
+      ;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
+
+  var numberList = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8];
+  var numberList2 = shuffle(numberList);
+
+  // Add a new div with number from shuffled array to all
+  // divs with .back class.
+  $('.back').each(function (index) {
+    $(this).html('<div class="back-image">' + numberList2[index]+ '</div>');
+  });
+  
+  //Enable jQuery flip plugin
   $(".game-square").flip();
 
 });
